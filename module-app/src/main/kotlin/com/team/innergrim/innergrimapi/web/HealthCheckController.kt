761bed773738807860ab1702e5b59d626540d999
woopi1030 <lozenkrantz@gmail.com>
2024-09-23 01:55:12 +0900

    [ADD] Health Check API Swagger Tag

--- module-app/src/main/kotlin/com/team/innergrim/innergrimapi/web/HealthCheckController.kt
+++ module-app/src/main/kotlin/com/team/innergrim/innergrimapi/web/HealthCheckController.kt
@@ -1,11 +1,13 @@
 package com.team.innergrim.innergrimapi.web
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/health")
 @RestController
+@Tag(name = "health")
 class HealthCheckController {
 
     @GetMapping
