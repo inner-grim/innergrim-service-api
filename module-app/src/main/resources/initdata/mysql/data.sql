cc690357668ed74e069d7759a7bc4c997e64afa7
woopi1030 <lozenkrantz@gmail.com>
2024-09-26 01:55:23 +0900

    [ADD] dev mysql 초기 테이블, 데이터 생성 sql 추가

---
+++ module-app/src/main/resources/initdata/mysql/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO membership (name) values ('NORMAL');
+INSERT INTO membership (name) values ('VIP');
+
+INSERT INTO member (name, membership_id) values ('회원1', 1);
+INSERT INTO member (name, membership_id) values ('회원2', 2);
