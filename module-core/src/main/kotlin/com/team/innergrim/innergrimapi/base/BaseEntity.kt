58f6d3b22838f699c0515f3938fd64da65c2c739
woopi1030 <lozenkrantz@gmail.com>
2024-10-08 23:57:07 +0900

    [CHG] BaseEntity 수정

--- module-core/src/main/kotlin/com/team/innergrim/innergrimapi/base/BaseEntity.kt
+++ module-core/src/main/kotlin/com/team/innergrim/innergrimapi/base/BaseEntity.kt
@@ -4,9 +4,11 @@ import jakarta.persistence.*
 import org.hibernate.annotations.Comment
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity {
 
     @Id
