7314d28f1df7f6fe642c04154cca1a01c09d691a
woopi1030 <lozenkrantz@gmail.com>
2024-10-14 23:19:41 +0900

    [CHG] DEV 초기 데이터 수정

--- module-app/src/main/resources/initdata/mysql/data.sql
+++ module-app/src/main/resources/initdata/mysql/data.sql
@@ -5,3 +5,5 @@ INSERT INTO membership (name) values ('VIP');
 --INSERT INTO member (name, membership_id) values ('회원1', 1);
 --INSERT INTO member (name, membership_id) values ('회원2', 2);
 
+insert into member (membership_id, social_type, social_id, name, email, birth_date, gender, phone_number, ci, profile_image)
+values (1,'kakao','00000001','테스트유저','test@test.com','20000101','male','01011112222','11111',null);
