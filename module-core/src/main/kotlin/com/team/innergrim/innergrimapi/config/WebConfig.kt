6511dff12f169518d3ee34a2d8a342b34324a90a
woopi1030 <lozenkrantz@gmail.com>
2024-12-31 18:48:13 +0900

    [ADD] cms local, dev cors config

--- module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/WebConfig.kt
+++ module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/WebConfig.kt
@@ -4,6 +4,7 @@ import com.team.innergrim.innergrimapi.filter.ApiLoggingFilter
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -26,5 +27,12 @@ class WebConfig(
     }
 
 
-
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+            .allowedOrigins("http://localhost:3000") // 허용할 Origin
+            .allowedOrigins("http://innergrim-cms:8000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+            .allowedHeaders("*") // 모든 헤더 허용
+            .allowCredentials(true) // 쿠키 허용
+    }
 }
