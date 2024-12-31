9295e2af4e47084ddcf5a1e5268635b21e635b55
woopi1030 <lozenkrantz@gmail.com>
2024-12-31 18:48:33 +0900

    [ADD] batch sample

---
+++ module-api/src/main/kotlin/com/team/innergrim/innergrimapi/batch/SamplePrintJob.kt
@@ -0,0 +1,34 @@
+package com.team.innergrim.innergrimapi.batch
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+import org.springframework.transaction.PlatformTransactionManager
+
+@Component
+@EnableBatchProcessing
+class SamplePrintJob  {
+
+    @Bean
+    fun job (jobRepository: JobRepository, step1: Step): Job {
+        return JobBuilder("samplePrintJob", jobRepository)
+            .start(step1)
+            .build()
+    }
+
+    @Bean
+    fun step1(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
+        return StepBuilder("step1", jobRepository)
+            .tasklet({ contribution, chunkContext ->
+                println("hello world job!!!")
+                RepeatStatus.FINISHED // 작업이 성공적으로 완료되었음을 알림
+            }, transactionManager)
+            .build()
+    }
+}
