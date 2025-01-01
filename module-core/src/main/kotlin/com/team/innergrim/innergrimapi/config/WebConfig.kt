9aac9ee907cef8793bb37006d29f004398d06e29
woopi1030 <lozenkrantz@gmail.com>
2025-01-01 18:42:55 +0900

    [CHG] cors 허용 list 수정

--- module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/WebConfig.kt
+++ module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/WebConfig.kt
@@ -31,7 +31,9 @@ class WebConfig(
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
             .allowedOrigins(
                 "http://localhost:3000",
-                "http://innergrim-cms:8000"
+                "http://http://3.39.72.21/:3000",
+                "http://innergrim-cms:3000",
+                "https://cms.innergrim.info"
             ) // 허용할 Origin
             .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 모든 헤더 허용
