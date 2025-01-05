d4656daefbfff8f9956a643761b6d80223a89335
woopi1030 <lozenkrantz@gmail.com>
2025-01-05 20:17:49 +0900

    [CHG] embeded redis local에서만 돌게 수정

--- module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/EmbededRedisConfig.kt
+++ module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/EmbededRedisConfig.kt
@@ -3,11 +3,13 @@ package com.team.innergrim.innergrimapi.config
 import jakarta.annotation.PostConstruct
 import jakarta.annotation.PreDestroy
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import redis.embedded.RedisServer
 import java.io.IOException
 import java.net.ServerSocket
 
 @Configuration
+@Profile("local")
 class EmbededRedisConfig {
 
     private lateinit var redisServer: RedisServer
