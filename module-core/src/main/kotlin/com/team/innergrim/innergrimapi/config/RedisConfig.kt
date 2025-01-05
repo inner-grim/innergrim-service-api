d7cd5f652759127daf078e6b2133421cc233d033
woopi1030 <lozenkrantz@gmail.com>
2025-01-05 20:42:29 +0900

    [CHG] redis 설정 변경

--- module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/RedisConfig.kt
+++ module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/RedisConfig.kt
@@ -1,19 +1,27 @@
 package com.team.innergrim.innergrimapi.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @Configuration
 @EnableRedisRepositories
-class RedisConfig {
+class RedisConfig (
+    @Value("\${spring.redis.host}") private val redisHost: String,
+    @Value("\${spring.redis.port}") private val redisPort: String
+) {
 
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory()
+        val redisConfig = RedisStandaloneConfiguration()
+        redisConfig.hostName = redisHost
+        redisConfig.port = redisPort.toInt()
+        return LettuceConnectionFactory(redisConfig)
     }
 
     @Bean
