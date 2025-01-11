9bd5b659d2f269e8cb6a8f57fbe9e8e1df5576c8
woopi1030 <lozenkrantz@gmail.com>
2025-01-12 00:55:24 +0900

    [ADD] batch config 추가, DDL 파일 추가

--- module-api/src/main/resources/initdata/prd/schema.sql
+++ module-api/src/main/resources/initdata/prd/schema.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS picture_diary;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS member;
---DROP TABLE IF EXISTS admin;
---DROP TABLE IF EXISTS profile;
 
 CREATE TABLE IF NOT EXISTS picture_diary (
     id bigint auto_increment primary key comment 'ID',
@@ -44,28 +42,3 @@ CREATE TABLE IF NOT EXISTS member (
     created_at datetime null default current_timestamp comment '생성 시간',
     updated_at datetime null comment '수정 시간'
 ) comment '멤버' CHARSET=utf8mb4;
-
---CREATE TABLE IF NOT EXISTS admin (
---    id bigint auto_increment primary key comment 'ID',
---    role_id bigint not null comment '역할',
---    login_id varchar(50) not null comment '로그인 시 아이디',
---    password varchar(255) not null unique comment '로그인 시 비밀번호',
---    name varchar(50) not null comment '이름',
---    email varchar(255) null comment '이메일',
---    birth_date varchar(8) null comment '생년월일',
---    phone_number varchar(20) not null comment '전화번호',
---    status varchar(20) not null default 'active' comment '상태',
---    created_at datetime null default current_timestamp comment '생성 시간',
---    updated_at datetime null comment '수정 시간'
---) comment '관리자' CHARSET=utf8mb4;
-
---CREATE TABLE IF NOT EXISTS profile (
---    id bigint auto_increment primary key comment 'ID',
---    member_id bigint not null comment '멤버 ID',
---    nick_name varchar(255) not null comment '닉네임',
---    gender varchar(20) null comment '성별',
---    profile_image varchar(255) null comment '프로필 이미지',
---    status varchar(20) not null default 'active' comment '상태',
---    created_at datetime null default current_timestamp comment '생성 시간',
---    updated_at datetime null comment '수정 시간'
---) comment '프로필' CHARSET=utf8mb4;
---
+++ module-core/src/main/kotlin/com/team/innergrim/innergrimapi/config/BatchConfig.kt
@@ -0,0 +1,11 @@
+package com.team.innergrim.innergrimapi.config
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+@EnableBatchProcessing
+class BatchConfig {
+
+}
