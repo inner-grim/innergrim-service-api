2674f05a5d8964aca64059004b8eaddc54592a1b
woopi1030 <lozenkrantz@gmail.com>
2025-01-14 22:03:12 +0900

    [CHG] healch check

--- module-domain-health-check/src/main/kotlin/com/team/innergrim/innergrimapi/service/HealthCheckDomainService.kt
+++ module-domain-health-check/src/main/kotlin/com/team/innergrim/innergrimapi/service/HealthCheckDomainService.kt
@@ -1,13 +1,19 @@
 package com.team.innergrim.innergrimapi.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class HealthCheckDomainService () {
+class HealthCheckDomainService(
+    @Value("\${profile}") private val profile: String
+) {
 
     // ::::: [GET] :::::
 
-    fun getHealthCheck (): String {
-        return "::::: Health Check Ok! :::::"
+    fun getHealthCheck(): String {
+        return """ 
+                ::::: Health Check Ok! :::::
+                1. profile : ${profile}
+                """.trimIndent()
     }
 }
